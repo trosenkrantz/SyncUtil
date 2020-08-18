9e0b7d6e7d3db91a63067be7ec346057dbb51129
Tobias Rosenkrantz Gundersen <gundersen.tobias@gmail.com>
2020-08-18 17:25:58 +0200

    Updated JDoc

--- src/main/java/com/github/trosenkrantz/sync/util/runnalbe/SingleRunnableManager.java
+++ src/main/java/com/github/trosenkrantz/sync/util/runnalbe/SingleRunnableManager.java
@@ -2,22 +2,34 @@ package com.github.trosenkrantz.sync.util.runnalbe;
 
 /**
  * A handler to only run once.
- * Here you at run-time specifies what runnable to run.
+ * You specify at run-time what runnable to run.
+ * <p>
+ * You can suspend this.
+ * If you call {@link #run(Runnable)} while suspended, it will be scheduled to run once {@link #allow()} is called.
+ * Only the first scheduled {@link Runnable} is run.
  */
 public class SingleRunnableManager {
     private volatile RunnableState state;
     private Runnable scheduledRunnable;
 
+    /**
+     * Constructs this and sets it to not suspended.
+     */
     public SingleRunnableManager() {
         this(true);
     }
 
+    /**
+     * Constructs this and specific if allowed or suspended.
+     *
+     * @param allowed true if allowed, false if suspended
+     */
     public SingleRunnableManager(boolean allowed) {
         state = allowed ? RunnableState.IDLE : RunnableState.SUSPENDED;
     }
 
     /**
-     * If this is the first call to this method in this instance, the runnable is run.
+     * If this is the first call to this method in this instance, the runnable is run or scheduled (if suspended).
      * Otherwise, this method does nothing.
      *
      * @param runnable the runnable
@@ -41,6 +53,10 @@ public class SingleRunnableManager {
         runnable.run();
     }
 
+    /**
+     * Allows this to run a runnable.
+     * If a {@link Runnable} was scheduled, runs it.
+     */
     public void allow() {
         synchronized (this) {
             switch (state) {
@@ -59,6 +75,9 @@ public class SingleRunnableManager {
         scheduledRunnable.run();
     }
 
+    /**
+     * Suspends this, not allowing {@link Runnable}'s to run until {@link #allow()} is called.
+     */
     public synchronized void suspend() {
         switch (state) {
             case IDLE:
